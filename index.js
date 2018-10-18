3c8b9ac547d8873ee5988d963fac6c732bdcd797
godseyeview <godseyeview@gmail.com>
2018-10-17 20:17:13 -0400

    change port as to not conflict with the react page

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var Titles = require('./models/models'); //created model loading here
 var  mongoose = require('mongoose');
 
 var app = express();
+const port = process.env.PORT || 5000;
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://readonly:turner@ds043348.mongolab.com:43348/dev-challenge'); 
@@ -13,6 +14,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var routes = require('./routes/routes'); //importing route
 routes(app);
 
-var server = app.listen(3000, function () {
+var server = app.listen(port, function () {
     console.log("app running on port.", server.address().port);
 });
